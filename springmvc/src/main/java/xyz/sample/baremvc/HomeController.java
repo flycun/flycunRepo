b4c2707d234d1c79b432e84055e4c4c5f4abebad
wyc <wuyicun0402@163.com>
2013-05-12 23:12:32 +0800

    update

--- springmvc/src/main/java/xyz/sample/baremvc/HomeController.java
+++ springmvc/src/main/java/xyz/sample/baremvc/HomeController.java
@@ -1,33 +1,39 @@
-package xyz.sample.baremvc;  
+package xyz.sample.baremvc;
 
-   
+import java.util.Comparator;
 
-import org.springframework.stereotype.Controller;  
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import org.springframework.web.bind.annotation.RequestMapping;  
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
 
-   
+	@Autowired
+	Comparator<String> comparator;
 
-/**  
+	@RequestMapping(value = "/")
+	public String home() {
+		System.out.println("HomeController: Passing through...");
+		return "home";
+	}
 
- * Handles requests for the application home page.  
+	@RequestMapping(value = "/compare", method = RequestMethod.GET)
+	public String compare(@RequestParam("input1") String input1,
+			@RequestParam("input2") String input2, Model model) {
 
- */ 
+		int result = comparator.compare(input1, input2);
+		String inEnglish = (result < 0) ? "less than" : (result > 0 ? "greater than" : "equal to");
 
-@Controller 
+		String output = "According to our Comparator, '" + input1 + "' is " + inEnglish + "'" + input2 + "'";
 
-public class HomeController {  
-
-   
-
-    @RequestMapping(value = "/")  
-
-    public String home() {  
-
-        System.out.println("HomeController: Passing through...");  
-
-        return "WEB-INF/views/home.jsp";  
-
-    }  
-
-} 
+		model.addAttribute("output", output);
+		return "compareResult";
+	}
+}
